313fcf26b7d99744ecdc7db14a938847a2ba7220
cyang-05 <chiashiiyang@gmail.com>
2025-09-26 15:21:06 -0400

    phase 2

--- A3-simple/src/SLL.java
+++ A3-simple/src/SLL.java
@@ -6,5 +6,61 @@
  */
 
  public class SLL<T> {
-  
+
+
+    
+
+
+
+
+  // Phase 2
+
+  /** 
+   *  Inserts the given item at the tail of the list
+   *  @param item to insert 
+   */
+  public void addLast(T v);
+
+
+  // NOT RETURNING ANYTHING so will be reassigning linkedList variable to 
+  /** 
+   *  Inserts the given item after the specified node
+   *  @param here node to insert after
+   *  @param v item to insert 
+   */
+  public void addAfter(NodeSL<T> here, T v) {
+     
+  }
+
+  /** 
+   *  Removes the given item from the head of the list
+   *  @return v item removed
+   */
+  public T removeFirst();
+
+  /** 
+   *  Removes the given item from the tail of the list
+   *  @return item removed
+   */
+  public T removeLast();
+
+  /** 
+   *  Removes the node after the given position
+   *  @param here marks position to remove after
+   *  @return item removed
+   */
+  public T removeAfter(NodeSL<T> here) {
+    return T;
+
+  }
+
+  /**
+   *  Returns a count of the number of elements in the list
+   *  @return current number of nodes
+   */
+  public int size() {
+    return 0;
+  }
+
+
  }
